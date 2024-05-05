e8dbd2c2643b12300475685274d70b14c28a15b9
vineethkoppai@gmail.com <vineethkoppai@gmail.com>
2024-05-05 17:23:21 +0530

    Update footer CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,14 +83,14 @@ export default function Home() {
       </div>
 
       {/* Feature section */}
-      <div className='mx-auto mb-32 mt-32 max-w-5xl sm:mt-56 w-4/5'>
+      <div className='mx-auto mb-32 mt-32 max-w-5xl sm:mt-56'>
         <div className='mb-12 px-6 lg:px-8'>
           <div className='mx-auto max-w-2xl sm:text-center'>
             <h2 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
               Start chatting in minutes
             </h2>
             <p className='mt-4 text-lg text-gray-600'>
-              Chatting to your PDF files has never been
+              Chatting to your PDF or Text files has never been
               easier than with ConvoDocs.
             </p>
           </div>
